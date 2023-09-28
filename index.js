04bbc5b7cee11bc35bce68c4b8fec6ed8d3f6114
Karl-Erik Gustafsson <ke.gustafsson@gmail.com>
2023-09-28 19:23:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { readFile, writeFile } = require('fs/promises');
 const { join } = require('path');
-const lockfile = require('proper-lockfile');
 
 module.exports = function createPlugin(app) {
   const plugin = {};
@@ -8,32 +7,19 @@ module.exports = function createPlugin(app) {
   plugin.name = 'SignalK Engine Hours';
   plugin.description = 'Tbd';
 
-  let engines = {};
-  let state;
+  let engines = { paths: [] };
   let unsubscribes = [];
   const setStatus = app.setPluginStatus || app.setProviderStatus;
 
-  const retryOptions = {
-    retries: {
-        retries: 5,
-        factor: 3,
-        minTimeout: 1 * 1000,
-        maxTimeout: 60 * 1000,
-        randomize: true,
-    }
-  };
-  
   plugin.start = function start(options) {
     const enginesFile = join(app.getDataDirPath(), 'engines.json');
-    readFile(enginesFile, 'utf-8')
-      .catch((e) => {
-        engines = {
-          paths: []
-        };
-        writeFile(enginesFile, JSON.stringify({
-          engines,
-        }), 'utf-8')
-      });
+    try {
+      readFile(enginesFile, 'utf-8')
+        .then((content) => JSON.parse(content))
+        .then((data) => {
+          engines = data.engines;
+        })
+    } catch (error) { }
 
     const subscription = {
       context: 'vessels.self',
@@ -72,7 +58,7 @@ module.exports = function createPlugin(app) {
       subscription,
       unsubscribes,
       (subscriptionError) => {
-        app.error(`Error:${subscriptionError}`);
+        app.error(`Error: ${subscriptionError}`);
       },
       (delta) => {
         if (!delta.updates) {
@@ -83,48 +69,27 @@ module.exports = function createPlugin(app) {
             return;
           }
           u.values.forEach((v) => {
-            const path = v.path;
-            const value = v.value;
-
-            lockfile.lock(enginesFile, retryOptions)
-              .then((release) => {
-                readFile(enginesFile, 'utf-8')
-                  .then((content) => JSON.parse(content))
-                  .then((data) => {
-                    const pathObject = data.engines.paths.find((item) => item.path === path);
-                    if (!pathObject) {
-                      data.engines.paths.push(
-                        {
-                          path: path,
-                          state: null,
-                          time: new Date().toISOString(),
-                        }
-                      );
-                      const engines = data.engines
-                      writeFile(enginesFile, JSON.stringify({
-                        engines,
-                      }), 'utf-8')
-                    }
-                    if (pathObject) {
-                      pathObject.state = pathObject.state + options.updateRate;
-                      pathObject.time = new Date().toISOString();
-                      console.log(JSON.stringify(data.engines, null, 2))
-                      const engines = data.engines
-                      writeFile(enginesFile, JSON.stringify({
-                        engines,
-                      }), 'utf-8')
-                    }
-                  })
-                  .catch((e) => {
-                  });
-                return release();
-              })
-              .catch((e) => {
-                console.error(e)
-              }); 
+            const pathObject = engines.paths.find((item) => item.path === v.path);
+            if (!pathObject) {
+              engines.paths.push(
+                {
+                  path: v.path,
+                  state: null,
+                  time: new Date().toISOString(),
+                }
+              );
+            }
+            if (pathObject) {
+              pathObject.state = pathObject.state + options.updateRate;
+              pathObject.time = new Date().toISOString();
+            }
+            writeFile(enginesFile, JSON.stringify({
+              engines,
+            }), 'utf-8')
+            app.debug(engines)
           });
         });
-      },
+      }
     );
   };
 
